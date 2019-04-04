fb72036a3c3651923e48b6318aa35e1b901738f8
lzzhaha <zlinai@connect.ust.hk>
2019-04-04 16:11:48 +0800

    fix tab error

--- scripts/fitnets_training.py
+++ scripts/fitnets_training.py
@@ -34,34 +34,34 @@ def generateConvRegressor(teacher_hintlayer, student_layer):
         
   if isinstance(teacher_hintlayer, MaxoutConvC01B):
     hint_reg_layer = MaxoutConvC01B(num_channels=out_ch, 
-				    num_pieces=teacher_hintlayer.num_pieces, 
-				    kernel_shape=ks, 
-				    pool_shape=[1,1], 
-				    pool_stride=[1,1], 
-				    layer_name=layer_name, 
-				    irange=irng,  
-				    max_kernel_norm=mkn, 
-				    tied_b=teacher_hintlayer.tied_b)
+            num_pieces=teacher_hintlayer.num_pieces, 
+            kernel_shape=ks, 
+            pool_shape=[1,1], 
+            pool_stride=[1,1], 
+            layer_name=layer_name, 
+            irange=irng,  
+            max_kernel_norm=mkn, 
+            tied_b=teacher_hintlayer.tied_b)
 
   elif isinstance(teacher_hintlayer, ConvRectifiedLinear):
     nonlin = RectifierConvNonlinearity()
     hint_reg_layer = ConvElemwise(output_channels = out_ch,
-				  kernel_shape = ks,
-				  layer_name = layer_name,
-				  nonlinearity = nonlin,
-				  irange = irng,
-				  max_kernel_norm = mkn,
-				  tied_b = tb)
-				  
+          kernel_shape = ks,
+          layer_name = layer_name,
+          nonlinearity = nonlin,
+          irange = irng,
+          max_kernel_norm = mkn,
+          tied_b = tb)
+          
   elif isinstance(teacher_hintlayer, ConvElemwisePL2):
     nonlin = teacher_hintlayer.nonlinearity
     hint_reg_layer = ConvElemwisePL2(output_channels = out_ch,
-				  kernel_shape = ks,
-				  layer_name = layer_name,
-				  nonlinearity = nonlin,
-				  irange = irng,
-				  max_kernel_norm = mkn,
-				  tied_b = tb) 
+          kernel_shape = ks,
+          layer_name = layer_name,
+          nonlinearity = nonlin,
+          irange = irng,
+          max_kernel_norm = mkn,
+          tied_b = tb) 
 
   else:
    raise AssertionError("Unknown convolutional layer type")
@@ -109,15 +109,15 @@ def fitnets_hints(student, fromto_student, teacher, hintlayer, regressor_type):
     # Add conv/fc regressor to the student subnetwork if needed
     if regressor_type == 'conv':
       if(teacher_output_space.shape < student_output_space.shape or (teacher_output_space.num_channels > student_output_space.num_channels and teacher_output_space.shape == student_output_space.shape)):
-	# Add convolutional regressor
-	hint_reg_layer = generateConvRegressor(teacher.layers[hintlayer], student.model.layers[-1])
-	hint_reg_layer.set_mlp(student.model)  
-	hint_reg_layer.set_input_space(student.model.layers[-1].output_space)
-	student.model.layers.append(hint_reg_layer)
+        # Add convolutional regressor
+        hint_reg_layer = generateConvRegressor(teacher.layers[hintlayer], student.model.layers[-1])
+        hint_reg_layer.set_mlp(student.model)  
+        hint_reg_layer.set_input_space(student.model.layers[-1].output_space)
+        student.model.layers.append(hint_reg_layer)
       elif (teacher_output_space.shape == student_output_space.shape and teacher_output_space.num_channels == student_output_space.num_channels):
-	pass
+        pass
       elif (teacher_output_space.shape > student_output_space.shape):
-	raise NotImplementedError('Convolutional regressor needs zero-padding, you may want to use a fully connected regressor instead')
+        raise NotImplementedError('Convolutional regressor needs zero-padding, you may want to use a fully connected regressor instead')
     elif regressor_type == 'fc':
       raise NotImplementedError('FC')
       # Add fully-connected regressor
@@ -126,7 +126,7 @@ def fitnets_hints(student, fromto_student, teacher, hintlayer, regressor_type):
       hint_reg_layer.set_input_space(student.model.layers[-1].output_space)
       student.model.layers.append(hint_reg_layer)
      # elif (teacher_output_space.shape == student_output_space.shape and teacher_output_space.num_channels == student_output_space.num_channels):
-#	pass
+# pass
     else:
       raise AssertionError('Regressor must be convolutional (conv) or fully-connected (fc)')
 
@@ -139,12 +139,12 @@ def fitnets_hints(student, fromto_student, teacher, hintlayer, regressor_type):
     # Change monitored channel
     if student.algorithm.monitoring_dataset.has_key('valid'):
       if isinstance(student.algorithm.termination_criterion,MonitorBased): 
-      	student.algorithm.termination_criterion._channel_name = "valid_cost_wrt_teacher"
+        student.algorithm.termination_criterion._channel_name = "valid_cost_wrt_teacher"
       
         # Change monitoring channel for best model    
         for ext in range(len(student.extensions)):
-	  if isinstance(student.extensions[ext],MonitorBasedSaveBest):
-	    student.extensions[ext].channel_name = "valid_cost_wrt_teacher"
+          if isinstance(student.extensions[ext],MonitorBasedSaveBest):
+            student.extensions[ext].channel_name = "valid_cost_wrt_teacher"
       elif isinstance(student.algorithm.termination_criterion,Or) or isinstance(student.algorithm.termination_criterion,And): 
         pos =next((i for i in range(len(student.algorithm.termination_criterion._criteria)) if isinstance(student.algorithm.termination_criterion._criteria[i],MonitorBased)), len(student.algorithm.termination_criterion._criteria)+1)
         if pos < len(student.algorithm.termination_criterion._criteria): 
@@ -157,8 +157,8 @@ def fitnets_hints(student, fromto_student, teacher, hintlayer, regressor_type):
 
     elif student.algorithm.monitoring_dataset.has_key('test'):
       if isinstance(student.algorithm.termination_criterion,MonitorBased): 
-        student.algorithm.termination_criterion._channel_name = "test_cost_wrt_teacher"	
-	 
+        student.algorithm.termination_criterion._channel_name = "test_cost_wrt_teacher" 
+   
       elif isinstance(student.algorithm.termination_criterion,Or) or isinstance(student.algorithm.termination_criterion,And):
         pos =next((i for i in range(len(student.algorithm.termination_criterion._criteria)) if isinstance(student.algorithm.termination_criterion._criteria[i],MonitorBased)), len(student.algorithm.termination_criterion._criteria)+1)
         if pos < len(student.algorithm.termination_criterion._criteria): 
@@ -244,7 +244,7 @@ def execute(student_yaml, regressor_type, hints_epochs=None, lr_scale=1.0):
     
   # FitNets Training Stage 1: Train guided layers with teacher hints 
   for i in range(n_hints):
-    print 'FitNets Training Stage 1: Training student guided layer %d out of %d' % (i+1, n_hints)
+    print('FitNets Training Stage 1: Training student guided layer %d out of %d' % (i+1, n_hints))
       
     # Select student block of layers forming subnetwork
     previous_guided = student_layers[i-1]+1 if i>0 else 0
@@ -269,8 +269,8 @@ def execute(student_yaml, regressor_type, hints_epochs=None, lr_scale=1.0):
     best_path = student_hint.save_path
     for ext in range(len(student_hint.extensions)):
       if isinstance(student_hint.extensions[ext],MonitorBasedSaveBest):
-	best_path = student_hint.extensions[ext].save_path
-	
+        best_path = student_hint.extensions[ext].save_path
+  
     fo = open(best_path, 'r')
     best_pretrained_model = pkl.load(fo)
     fo.close()
@@ -278,16 +278,16 @@ def execute(student_yaml, regressor_type, hints_epochs=None, lr_scale=1.0):
     # Save pretrained student Wguided into student model
     student.model.layers[0:current_guided+1] = best_pretrained_model.layers[0:current_guided+1]
 
-  print 'FitNets Training Stage 2: Training student softmax layer by means of KD'
+  print('FitNets Training Stage 2: Training student softmax layer by means of KD')
   
   for i in range(0,current_guided+1):
-	if student.model.layers[i].W_lr_scale is None:
-		student.model.layers[i].W_lr_scale = 1
-	if student.model.layers[i].b_lr_scale is None:
-		student.model.layers[i].b_lr_scale = 1		
-	
-	student.model.layers[i].W_lr_scale = student.model.layers[i].W_lr_scale*lr_scale
-	student.model.layers[i].b_lr_scale = student.model.layers[i].b_lr_scale*lr_scale  
+    if student.model.layers[i].W_lr_scale is None:
+      student.model.layers[i].W_lr_scale = 1
+    if student.model.layers[i].b_lr_scale is None:
+      student.model.layers[i].b_lr_scale = 1    
+  
+  student.model.layers[i].W_lr_scale = student.model.layers[i].W_lr_scale*lr_scale
+  student.model.layers[i].b_lr_scale = student.model.layers[i].b_lr_scale*lr_scale  
  
   student.main_loop()
   
@@ -296,7 +296,7 @@ def execute(student_yaml, regressor_type, hints_epochs=None, lr_scale=1.0):
  # for ext in range(len(softmax_hint.extensions)):
  #   if isinstance(softmax_hint.extensions[ext],MonitorBasedSaveBest):
  #     best_path = softmax_hint.extensions[ext].save_path
-	
+  
  # fo = open(best_path, 'r')
  # best_pretrained_model = pkl.load(fo)
  # fo.close()
